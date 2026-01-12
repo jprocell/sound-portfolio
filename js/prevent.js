00ef95a2a16136cff002db248d67f54bdfd9cf15
jprocell <procelljosh@gmail.com>
2026-01-11 23:47:56 -0600

    Removed redundant JS scripts. Integrated into Modal.JS or depricated features

--- js/prevent.js
+++
@@ -1,8 +0,0 @@
-// Prevents right clicking and downloading audio files
-document.addEventListener("DOMContentLoaded", () => {
-  const audioPlayers = document.querySelectorAll(".audio-player");
-  audioPlayers.forEach(player => {
-    player.addEventListener("contextmenu", e => e.preventDefault());
-  });
-});
-
